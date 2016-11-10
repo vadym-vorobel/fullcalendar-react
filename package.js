c450b7804ee202d893fff78ce31344b0f946ed79
alonoslav <vadym.vorobel@gmail.com>
2016-11-10 17:05:35 +0200

    Add reactive events refetching

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jss:fullcalendar-react',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'React wrapper for fullcalendar',
   git: 'https://github.com/alonoslav/fullcalendar-react',
   documentation: 'README.md',
